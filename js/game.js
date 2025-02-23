7d41013904d10a4d15b556385ce7bdf7e96ffca7
your_username <malahat.m82@gmail.com>
2025-02-23 20:55:41 +0100

    function formatData  is created

--- js/game.js
+++ js/game.js
@@ -1,11 +1,33 @@
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
 const URL="https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData="null";
+const formatData = (questionData)=>{
+	console.log(questionData);
+	const result = questionData.map((item)=>{
+		const questionObject = { question:item.question};
+		const answers = [...item.incorrect_answers];
+		const correctAnswerIndex =Math.floor(Math.random()*4);
+		answers.splice(correctAnswerIndex,0,item.correct_answer);
+		questionObject.answers = answers;
+		questionObject.correctAnswerIndex = correctAnswerIndex;
+		return questionObject;
+
+	});
+	return result;
+};
+
 const fetchData=async()=>{
 	const response=await fetch(URL);
 	const json= await response.json();
-	console.log(json);
-	StaticRange()
+	formattedData = formatData(json.results);
+	start();
+
 };
 const start=()=>{
-	
+	loader.style.display="none";
+	container.style.display="block";
+
 }
 window.addEventListener("load",fetchData);
