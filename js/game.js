d5e8721e486848b6406de6fa266f23e9c12f4d65
your_username <malahat.m82@gmail.com>
2025-02-23 21:06:16 +0100

    function formatData moved to helper.js

--- js/game.js
+++ js/game.js
@@ -1,22 +1,11 @@
+import formatData from "./helper.js";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 
 const URL="https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData="null";
-const formatData = (questionData)=>{
-	console.log(questionData);
-	const result = questionData.map((item)=>{
-		const questionObject = { question:item.question};
-		const answers = [...item.incorrect_answers];
-		const correctAnswerIndex =Math.floor(Math.random()*4);
-		answers.splice(correctAnswerIndex,0,item.correct_answer);
-		questionObject.answers = answers;
-		questionObject.correctAnswerIndex = correctAnswerIndex;
-		return questionObject;
 
-	});
-	return result;
-};
 
 const fetchData=async()=>{
 	const response=await fetch(URL);
